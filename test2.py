cebcb7942d19a7abc43e97908a4eb56d516a5323
HelloPyunny <pyunhs0121@gmail.com>
2025-02-10 22:01:17 -0600

    final changes

--- test2.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(5)
-pyautogui.typewrite('hello world this is a text file', interval=0.1)
--- tinker_test.py
+++ tinker_test.py
@@ -1,2 +1,4 @@
+# tinker tester to check if tinker is working properly
+
 import tkinter
 print(tkinter.Tk().tk.call('info', 'patchlevel'))
